9f432ba836a978689cf8b78944085195d4f1bb39
aashlay singla <aashlaysingla@gmail.com>
2019-10-11 15:00:28 +0530

    third commit

--- weather_app/playground/promise.js
+++ weather_app/playground/promise.js
@@ -1,7 +1,36 @@
-var somePromise = new Promise((resolve, reject) => {
-  resolve('Hey. it worked');
-});
+var asyncAdd = (a, b) => {
+  console.log(a," ",b);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof a === 'number' && typeof b === 'number') {
+        resolve(a + b);
+      } else {
+        reject('Arguments must be numbers');
+      }
+    }, 1500);
+  });
+};
+//
+// var somePromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('Hey. it worked');
+//     reject('Unable to fulfill promise');
+//   }, 2500);
+// });
+//
+// somePromise.then((message) => {
+//   console.log('Success: ',message);
+// }, (errorMessage) => {
+//   console.log('Error: ',errorMessage);
+// });
 
-somePromise.then((message) => {
-  console.log('Success: ',message);
+asyncAdd(5,'7').then((res) => {
+  console.log('Results: ',res);
+  return asyncAdd(res, 33);
+}, (errorMessage) => {
+  console.log('Error: ',errorMessage);
+}).then((res) => {
+  console.log('Should be 45',res);
+}, (errorMessage) => {
+  console.log(errorMessage);
 });
